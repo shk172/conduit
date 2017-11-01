4aaef94077f102ce5c0ae5de7cf8eb41a90b19a3
shk172 <sangkim2017@u.northwestern.edu>
2017-10-31 20:22:09 -0400

    Updating with Alpha Vantage API for real time quotes
    Updates every minute, will need to find a way to minimize the data
    coming in with json because it loads 100 data points each time it's
    called. Also AV doesn't give the company name??? The only advantage to
    it so far is the detailed price point.

--- app/components/Home.js
+++ app/components/Home.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import styles from './Home.css';
 
 const storage = require('electron-json-storage');
-
+const apiKey = "K2KAC8WYMD2CQHI5"
 export default class Home extends Component {
   constructor(props){
     super(props);
@@ -45,8 +45,10 @@ export default class Home extends Component {
 
   addTicker(e){
     e.preventDefault();
+    let avapi = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&interval=1min&apikey=K2KAC8WYMD2CQHI5&symbol=";
     let api = "http://dev.markitondemand.com/MODApis/Api/v2/Quote/json?symbol=";
     var url = api + this.state.newTicker;
+    let avurl = avapi + this.state.newTicker;
     fetch(url).then(response => {
       if(response.ok){
         return response.json();
@@ -54,7 +56,16 @@ export default class Home extends Component {
       throw new Error("Error fetching stock data");
     }).then(json => {
       if(json.Message === undefined){
-        console.log(json);
+      	json.Price = {};
+		    fetch(avurl).then(response=>{
+		    	if(response.ok){
+		    		return response.json();
+		    	}
+		    	throw new Error("Error fetching stock price");
+		    }).then(prices=>{
+			    let lastTime = prices['Meta Data']['3. Last Refreshed'];
+			    json.Price = prices['Time Series (1min)'][lastTime];
+		    })      
         let tickers = this.state.tickers;
         tickers.push(json);
         storage.set(json.Symbol, json, function(error){
@@ -62,7 +73,7 @@ export default class Home extends Component {
         });
         this.setState({
           tickers: tickers,
-          currentPage: tickers.slice(this.state.page, this.state.page + 5),
+          currentPage: tickers.slice(this.state.page * 5, (this.state.page*5) + 5),
           newTicker: "",
         });
       }
@@ -91,8 +102,13 @@ export default class Home extends Component {
 
   nextPage(){
   	let page = this.state.page;
+  	if(this.state.tickers.length <= 5){
+  		alert("No more page after this");
+  		return;
+  	}
   	if(this.state.tickers.length - (page * 5) < 0){
   		alert("No more page after this");
+  		return;
   	}
   	else{
   		page++;
@@ -120,7 +136,7 @@ export default class Home extends Component {
         <tr key={ticker.Symbol}>
           <td width="25%">{ticker.Symbol}</td>
           <td width="50%">{ticker.Name}</td>
-          <td width="25%">{ticker.LastPrice}</td>
+          <td width="25%">{ticker.Price['4. close']}</td>
         </tr>
       )
     })
